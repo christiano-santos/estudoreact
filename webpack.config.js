8b2d59f05ff8979a299b8a49ba1dd0fe971a2f46
christiano-santos <christiano_94oliveira@hotmail.com>
2021-12-16 22:13:55 -0300

    Adicionado cross-env, style-loadedr, css-loader e node-sass, todos os mesmos foram configurados no projeto.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { webpack } = require('webpack');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    mode: 'development',
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -11,6 +13,9 @@ module.exports = {
     },
     resolve: {
         extensions: ['.js','.jsx'],
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'public'),
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -24,6 +29,11 @@ module.exports = {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
                 use: 'babel-loader',
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader','css-loader','sass-loader'],
             }
         ]
     }
